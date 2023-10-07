688e9bf44e33d6aa936a5011e4723217a05b53f2
Mert Senkardesler <mertsenkardesler@yahoo.com.tr>
2023-10-06 22:33:09 -0500

    project1

--- Project/custom/cpu-new-forward.cc
+++ Project/custom/cpu-new-forward.cc
@@ -7,7 +7,6 @@ void conv_forward_cpu(float *output, const float *input, const float *mask, cons
     The code in 16 is for a single image.
     We have added an additional dimension to the tensors to support an entire mini-batch
     The goal here is to be correct, not fast (this is the CPU implementation.)
-
     Function paramters:
     output - output
     input - input
@@ -19,12 +18,13 @@ void conv_forward_cpu(float *output, const float *input, const float *mask, cons
     W - input width dimension
     K - kernel height and width (K x K)
     S - stride step length
-    */
-
+  */
   const int H_out = (H - K)/S + 1;
   const int W_out = (W - K)/S + 1;
-  
-
+  int cell_height = 0;
+  int cell_width = 0;
+  int inputHstart_index = 0;
+  int inputWstart_index = 0;
   // We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
   // An example use of these macros:
   // float a = in_4d(0,0,0,0)
@@ -32,10 +32,24 @@ void conv_forward_cpu(float *output, const float *input, const float *mask, cons
   #define out_4d(i3, i2, i1, i0) output[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
   #define in_4d(i3, i2, i1, i0) input[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
   #define mask_4d(i3, i2, i1, i0) mask[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
-
   // Insert your CPU convolution kernel code here
-
-  
+  for (int b = 0; b < B; ++b)       // for each image
+      for(int m = 0; m < M; ++m)   // for each output feature map
+          for(int h = 0; h < H_out; ++h) {  // for each output value (height)
+              inputHstart_index = h*S;
+              for(int w = 0; w < W_out; ++w){   // for each output value (width)
+                  inputWstart_index = w*S;
+                  out_4d(b, m, h, w) = 0.0f;   // initialize sum to 0
+                  for(int c = 0; c < C; ++c)   // sum over all input channels
+                      for(int p = 0; p < K; ++p){   // KxK filter (height)
+                          cell_height = inputHstart_index + p;
+                          for(int q = 0; q < K; ++q){   // KxK filter (width)
+                              cell_width = inputWstart_index + q;
+                              out_4d(b, m, h, w) += in_4d(b, c, cell_height, cell_width) * mask_4d(m, c, p, q);     
+                          }
+                      }
+              }
+          }
   #undef out_4d
   #undef in_4d
   #undef mask_4d
--- Project/custom/cpu-new-forward.h
+++ Project/custom/cpu-new-forward.h
@@ -1,6 +1,8 @@
 #ifndef SRC_LAYER_CPU_NEW_FORWARD_H
 #define SRC_LAYER_CPU_NEW_FORWARD_H
-
+#define out_4d(i3, i2, i1, i0) output[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+#define in_4d(i3, i2, i1, i0) input[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define mask_4d(i3, i2, i1, i0) mask[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 void conv_forward_cpu(float *output, const float *input, const float *mask, const int B, const int M, const int C, const int H, const int W, const int K, const int S = 1);
 
 #endif // SRC_LAYER_CPU_NEW_FORWARD_H
